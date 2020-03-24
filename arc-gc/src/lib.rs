16277295544086029d5cd3e4b725d5fafd3fea3a
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-03-24 15:53:17 +0800

    更

--- arc-gc/src/lib.rs
+++ arc-gc/src/lib.rs
@@ -1,4 +1,11 @@
 #![feature(get_mut_unchecked)]
+#[macro_use]
+extern crate lazy_static;
+use std::{
+    collections::BTreeMap,
+    ops::Deref,
+    sync::{Arc, Weak},
+};
 
 #[cfg(test)]
 mod tests {
@@ -8,16 +15,13 @@ mod tests {
     }
 }
 
-use std::{ops::Deref, sync::Arc};
-
 pub struct ArcGc<X>(Arc<Option<X>>);
-impl<X> ArcGc<X> {
+impl<X: Send + Sync> ArcGc<X> {
     pub fn new(x: X) -> Self {
         ArcGc(Arc::new(Some(x)))
     }
-    unsafe fn destory(&self) {
-        let mut this = self.0.clone();
-        *Arc::get_mut_unchecked(&mut this) = None;
+    pub fn downgrade(&self) -> WeakArcGc<X> {
+        WeakArcGc(Arc::downgrade(&self.0))
     }
 }
 impl<X> Deref for ArcGc<X> {
@@ -28,4 +32,39 @@ impl<X> Deref for ArcGc<X> {
             None => panic!("destoried ArcGc"),
         }
     }
+}
+pub unsafe trait AnyArcGc: Send + Sync {
+    unsafe fn destory(&self);
+    fn address(&self) -> usize;
+}
+unsafe impl<X: Send + Sync> AnyArcGc for ArcGc<X> {
+    unsafe fn destory(&self) {
+        *Arc::get_mut_unchecked(&mut self.0.clone()) = None;
+    }
+    fn address(&self) -> usize {
+        let pointer: *const Option<X> = &*self.0;
+        pointer as usize
+    }
+}
+pub struct WeakArcGc<X>(Weak<Option<X>>);
+impl<X: Send + Sync> WeakArcGc<X> {
+    pub fn new() -> WeakArcGc<X> {
+        WeakArcGc(Weak::new())
+    }
+    pub fn upgrade(&self) -> Option<ArcGc<X>> {
+        Some(ArcGc(self.0.upgrade()?))
+    }
+}
+pub unsafe trait AnyWeakArcGc: Send + Sync {
+    fn any_upgrade(&self) -> Option<Box<dyn AnyArcGc>>;
+}
+unsafe impl<X: 'static + Send + Sync> AnyWeakArcGc for WeakArcGc<X> {
+    fn any_upgrade(&self) -> Option<Box<dyn AnyArcGc>> {
+        Some(Box::new(self.upgrade()?))
+    }
+}
+
+lazy_static! {
+    static ref ALLOW_CYCLES_SET1: BTreeMap<usize, Box<dyn AnyWeakArcGc>> = BTreeMap::new();
+    static ref ALLOW_CYCLES_SET2: BTreeMap<usize, Box<dyn AnyWeakArcGc>> = BTreeMap::new();
 }
